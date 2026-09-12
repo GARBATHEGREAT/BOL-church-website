1c9d7b93ee62b06c5b9045dec67906919c0485fb
JOHN GARBA <86900662+GARBATHEGREAT@users.noreply.github.com>
2026-09-12 03:52:55 -0700

    Version 35: make church logo discoverable in search

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,13 @@ export const metadata: Metadata = {
   title: "Bread of Life Divine Covenant Ministry",
   description: "A place to belong. A people becoming like Christ.",
   icons: {
-    icon: "/logo.png",
-    shortcut: "/logo.png",
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-48.png", type: "image/png", sizes: "48x48" },
+      { url: "/favicon-192.png", type: "image/png", sizes: "192x192" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
   },
 };
 
